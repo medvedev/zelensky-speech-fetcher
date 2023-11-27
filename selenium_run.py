73855bb2012dc33a0d518fbfd550e65a83d5a8b3
Slava Medvediev <slava.medvedev@gmail.com>
2023-11-27 11:32:43 +0000

    Still testing

--- selenium_run.py
+++ selenium_run.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 print("Starting")
 chromium_options = Options()
 chromium_options.binary_location = "/usr/bin/chromium-browser"
-for arg in ['--headless', '--remote-debugging-port=9222']:
+for arg in ['--headless', '--remote-debugging-port=9222', '--disable-extensions']:
     chromium_options.add_argument(arg)
 driver = None
 
