7156b7cdda9d10a19b71c55f14455f9094197030
Slava Medvediev <slava.medvedev@gmail.com>
2023-12-14 23:52:35 +0000

    Stop adding column "__index_level_0__"

--- src/tools/dataset_cleanup.py
+++ src/tools/dataset_cleanup.py
@@ -19,30 +19,30 @@ dataset = load_dataset(REPO_ID, split='train', cache_dir='./.cache')
 ds = dataset.to_pandas()
 # ds['date'] = ds['date'].apply(convert_date)
 # ds_modified = False
-
-for language in ['en', 'uk']:
-    new_data = []
-    with open(f'speeches_{language}.jsonl', 'r', encoding='utf-8') as file:
-        for line in file:
-            new_data.append(json.loads(line))
-
-    # Convert the list of dictionaries to a Hugging Face dataset
-    new_dataset = Dataset.from_list(new_data)
-    new_ds = new_dataset.to_pandas()
-    new_ds['lang'] = 'language'
-
-    ds = pd.concat([ds, new_ds])
-
-ds_modified = True
+#
+# for language in ['en', 'uk']:
+#     new_data = []
+#     with open(f'speeches_{language}.jsonl', 'r', encoding='utf-8') as file:
+#         for line in file:
+#             new_data.append(json.loads(line))
+#
+#     # Convert the list of dictionaries to a Hugging Face dataset
+#     new_dataset = Dataset.from_list(new_data)
+#     new_ds = new_dataset.to_pandas()
+#     new_ds['lang'] = 'language'
+#
+#     ds = pd.concat([ds, new_ds])
+#
+# ds_modified = True
 
 # Remove duplicates:
-duplicates = ds[ds.duplicated(subset='date', keep=False)]
-if duplicates.empty:
-    print("No duplicates found in.")
-else:
-    ds.drop_duplicates(subset=['date', 'lang'], keep='first', inplace = True)
-    print("Duplicates removed and dataset saved.")
-    ds_modified = True
+# duplicates = ds[ds.duplicated(subset='date', keep=False)]
+# if duplicates.empty:
+#     print("No duplicates found in.")
+# else:
+#     ds.drop_duplicates(subset=['date', 'lang'], keep='first', inplace = True)
+#     print("Duplicates removed and dataset saved.")
+#     ds_modified = True
 
 
 # Remove row by index:
@@ -52,11 +52,10 @@ else:
 
 # Add lang column
 # ds['lang'] = 'uk'
-# ds_modified = True
+ds_modified = True
 
 
 if ds_modified:
-    # ds = ds.drop(columns=['__index_level_0__'])
-    ds = Dataset.from_pandas(ds)
+    ds = Dataset.from_pandas(ds, preserve_index=False)
     ds.push_to_hub(REPO_ID)
     print('Pushed successfully')
